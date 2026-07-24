08ebbf719297e3ccc963dd5ee250af058f67bceb
itsmanikandan73 <soulquiet8@gmail.com>
2026-07-24 08:44:55 +0530

    adding updated version of the program

--- old-programs/array_of_strings.c
+++ old-programs/array_of_strings.c
@@ -1,42 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-// Array of strings 
+#include <stdlib.h>
 
-int main(){
+int main(void) {
 
-   /* char fruits[][20] = {"Apple",
-                        "Banana",
-                        "Coconut",
-                        "pineapple"};
-    int size = sizeof(fruits)/ sizeof(fruits[0]);
+    char fruits[4][10] = {"apple","banana","coconut","pineapple"};
 
+    size_t rows = sizeof(fruits) / sizeof(fruits[0]);
 
-    fruits[0][0] = 'e';
-    fruits[0][4] = 'A';
-    
-    fruits[1][0] = 'a';
-    fruits[1][5] = 'B';
+    fruits[0][0] = 'A';
+    fruits[1][0] = 'B';
+    fruits[2][0] = 'C';
+    fruits[3][0] = 'p';
 
-    fruits[2][0] = 't';
-    fruits[2][6] = 'C';
-
-
-    for (int i = 0; i < size; i++){
-        printf("%s\n", fruits[i]);
-    }*/
-
-    char names[4][20] = {0};
-    int rows = sizeof(names) / sizeof(names[0]);
-    
-    for (int i = 0; i < rows; i++){
-        printf("Enter your name: ");
-        fgets(names[i],sizeof(names[i]), stdin);
-        names[i][strlen(names[i]) -1] = '\0';
-
+    for (int i = 0; i < rows; i++) {
+        fprintf(stdout,"%s\n", fruits[i]);
     }
 
-    for (int i = 0; i < rows; i++){
-        printf("%s\n", names[i]);
-    }   
-    return 0;
+    return EXIT_SUCCESS;
 }
