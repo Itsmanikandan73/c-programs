82a0a161e3f034a37185f21ad3433bb184965cf1
itsmanikandan73 <soulquiet8@gmail.com>
2026-08-02 16:10:21 +0530

    adding updated version of the program

--- old-programs/Arrays_user_input.c
+++ old-programs/Arrays_user_input.c
@@ -4,20 +4,20 @@
 // Garbage values | 
 
 int main(){
-
+    
     int scores[6] = {};
-
-
+    
+    
     for (int i = 0; i < 6; i++){
         printf("Enter your score: ");
-        scanf("%d", &scores[i]);
+        scanf("%d20s", &scores[i]);
     }
-
-
+    
+    
     for(int a = 0; a < 6; a++){
         printf("%d\n", scores[a]);
     }
-
-
+    
+    
     return 0;
 }
