a01f33e38c530790ac6f72923446db325c2c4ab2
itsmanikandan73 <soulquiet8@gmail.com>
2026-08-02 16:08:10 +0530

    adding updated version of array.c program

--- old-programs/array.c
+++ old-programs/array.c
@@ -1,33 +1,46 @@
 #include <stdio.h>
+// In this program we are going to checkout the one and two dimentional array
+void numbers(void);
+void vowels(void);
+void numbers2(void);
 
-	//one dimentional array
-	//two dimentional array
-
-int main()
-{
-	/*int num[4];
-
-	num[0]=2;
-	num[1]=4;
-	num[2]=6;
-	num[3]=8;
-
-	printf("Guess the number you want %d\n",num[2]);
-
-	int numbers[3][4]={
-	{1,2,3,4},
-	{5,6,7,8},
-	{9,10,11,12}};
-
-	printf("your wished numbers is %d \n",numbers[2][3]); */
-
-	char vowels[1][5]={
-
-	{'a','e','i','o','u'}};
-
-	printf("wished array is %c \n",vowels[0][4]);
+int main(int argc, char **argv) {
+	numbers();
+	printf("\n");
+	vowels();
+	numbers2();
 	
-
-
 	return 0;
+}
+
+void vowels(void) {
+	char vowels[1][5] = {{'a','e','i','o','u'}};
+	
+	for (int i = 0; i < 5; i++) {
+		fprintf(stdout, "%c   ", vowels[0][i]);
+	}
+	printf("\n");
+}
+
+void numbers(void) {
+	int numbers[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+	
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 4; j++) {
+			fprintf(stdout, "%d\t", numbers[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void numbers2(void) {
+	int number[5];
+	
+	number[0] = 2;
+	number[1] = 4;
+	number[2] = 6;
+	number[3] = 8;
+	number[4] = 10;
+	
+	fprintf(stdout,"\nThe number[3] = %d\n", number[3]);
 }
