5a47175c63e3f5c73ec3fd8bdd26c946668d3cad
itsmanikandan73 <soulquiet8@gmail.com>
2026-08-03 11:14:05 +0530

    adding updated version of break and continue program

--- old-programs/break_and_continue.c
+++ old-programs/break_and_continue.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// This program is used to showcase the break and continue
 //Break == Break out of the loop(STOP)
 //Contiue == skip current cycle of a loop (SKIP)
 
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        if (i == 5) continue;
+        else if (i == 9) break;
+
+        fprintf(stdout,"%d\n", i);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+/*
 int main(){
 
     for (int i = 1; i <= 10; i++){
@@ -20,3 +32,4 @@ int main(){
 
     return 0;
 }
+    */
